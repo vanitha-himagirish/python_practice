7da001d4f54699c982e4e0aadd29d95f2eb00d1a
vanitha-himagirish <55011879+vanitha-himagirish@users.noreply.github.com>
2024-05-04 17:42:54 -0500

    Add new item to list after a specified item

---
+++ lists/excercise_6.py
@@ -0,0 +1,14 @@
+#  Add new item to list after a specified item
+# Write a program to add item 7000 after 6000 in the following Python List
+
+# understand indexing
+# list1[0] = 10
+# list1[1] = 20
+# list1[2] = [300, 400, [5000, 6000], 500]
+# list1[2][2] = [5000, 6000]
+# list1[2][2][1] = 6000
+
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+
+list1[2][2].append(7000)
+print(list1)
